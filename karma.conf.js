e868c374f1939caa7cd99fbed5f4cfc5b5a31d13
Brandon Ward <brandon.ward1993@gmail.com>
2020-01-17 20:26:47 -0700

    Configure PR Builds / Add branch protections to 'master' (#1)
    * Change title to NGenius Gaming
    
    * Tweak allowed branches for the build
    
    * Install the ChromeDriver in the build
    
    * Add Chrome Headless --no-sandbox

--- karma.conf.js
+++ karma.conf.js
@@ -27,6 +27,16 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeNoSandbox: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      },
+      ChromeNoSandboxHeadless: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
   });
 };

1c329a8365061935870b07553e32a53792401acc
Brandon Ward <brandon.ward1993@gmail.com>
2020-01-20 20:44:37 -0700

    Add base configuration service

---
+++ src/app/config/configuration.model.ts
@@ -0,0 +1,9 @@
+/**
+ * The configuration interface for the project.
+ * Declaring it this way ensures that we have strong type-safety
+ * between configuration usage and actual configuration.
+ */
+export interface Configuration {
+  // placeholder for twitch configuration
+  twitch: object;
+}

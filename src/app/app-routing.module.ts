1e65c3ba1f87fb403675d6d91a05925c401d0ccf
Brandon Ward <brandon.ward1993@gmail.com>
2020-01-22 21:10:19 -0700

    Fix routing bug so that unknown paths always load the twitch dashboard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,21 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {TwitchDashboardComponent} from './twitch/twitch-dashboard/twitch-dashboard.component';
 
+const home = '/team-view/twitch';
 
 const routes: Routes = [
   {
     path: 'team-view',
     children: [
       {path: 'twitch', component: TwitchDashboardComponent},
-      // empty paths and unknown paths in this sub-tree should redirect to twitch.
-      {path: '', redirectTo: '/team-view/twitch', pathMatch: 'full' },
-      {path: '**', redirectTo: '/team-view/twitch', pathMatch: 'full'}
+      // empty paths and unknown paths in this sub-tree should redirect to home.
+      {path: '', redirectTo: home, pathMatch: 'full' },
+      {path: '**', redirectTo: home, pathMatch: 'full'}
     ]
   },
-  // empty paths and unknown paths should redirect to /team-view
-  {path: '', redirectTo: '/team-view', pathMatch: 'full'},
-  {path: '**', redirectTo: '/team-view', pathMatch: 'full'}
+  // empty paths and unknown paths should redirect to home
+  {path: '', redirectTo: home, pathMatch: 'full'},
+  {path: '**', redirectTo: home, pathMatch: 'full'}
 ];
 
 @NgModule({

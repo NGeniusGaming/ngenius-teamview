09196e086b847b0312f33222f670c2a266560035
Brandon Ward <brandon.ward1993@gmail.com>
2020-01-30 20:38:53 -0700

    Fixing the tests and making them resilient and better

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,26 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import {HeaderComponent} from './header.component';
 import {MatToolbarModule, MatTooltipModule} from '@angular/material';
 import {RouterTestingModule} from '@angular/router/testing';
+import {ConfigurationService} from '../config/configuration.service';
+import {Configuration} from '../config/configuration.model';
+import {first} from 'rxjs/operators';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
+  let configuration: Configuration;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [MatTooltipModule, MatToolbarModule, RouterTestingModule],
-      declarations: [ HeaderComponent ]
+      declarations: [HeaderComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(fakeAsync(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+    fixture.debugElement.injector.get(ConfigurationService)
+      .configuration()
+      .pipe(first())
+      .subscribe(value => configuration = value);
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -33,11 +42,13 @@ describe('HeaderComponent', () => {
 
   it('should have ngenius gaming as the title', () => {
     const title = fixture.debugElement.nativeElement.querySelector('#application-title').innerText;
-    expect(title).toBe('NGenius Gaming');
+    expect(title).toBe(configuration.root.applicationTitle);
   });
 
-  it('should have loaded an image for the logo', () => {
+  it('should have loaded the image for the logo', () => {
     const image = fixture.debugElement.nativeElement.querySelector('#application-logo').innerHTML;
-    expect(image.indexOf('.png')).toBeGreaterThan(0);
+    const imageHtml = new DOMParser().parseFromString(image, 'text/html');
+    const src = imageHtml.body.firstElementChild.attributes.getNamedItem('src').value;
+    expect(src).toBe(configuration.root.applicationLogo);
   });
 });

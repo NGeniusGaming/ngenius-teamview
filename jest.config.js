7b441dd97721e14ff0705a564438e7e96e28dbc3
Brandon Ward <brandon.ward1993@gmail.com>
2022-10-12 23:40:18 -0500

    Feature/upgrade angular (#60)
    * Adding an .nvmrc file
    
    * Initial angular 13 bump
    
    * Upgrade material to 13
    
    * Remove IE polyfils, which are not supported
    
    * Initial bump to angular 14
    
    * Upgrade angular material to 14
    
    * Ignore .angular/ directory
    
    * Upgrade jest-schematic
    
    * update angular-cli-ghpages
    
    * Upgrade dependent libs
    
    * Jest upgrades
    
    * Update jest-preset-angular
    
    * Downgrade jest and fix some security vulns
    
    * Fix jest config
    
    * Update runners
    
    * Downgrade to node 14?
    
    * Shard the build
    
    * Simplify jest configs
    
    * Back to node 16

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  moduleNameMapper: {
-    '@core/(.*)': '<rootDir>/src/app/core/$1',
-  },
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
 };

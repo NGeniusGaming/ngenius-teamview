891aff518183d022dad237fc0ff986dbec4d3fab
Brandon Ward <brandon.ward1993@gmail.com>
2022-10-12 22:32:20 -0600

    Simplify jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,6 @@
-const { pathsToModuleNameMapper } = require("ts-jest");
-const { paths } = require('./tsconfig.json').compilerOptions;
-
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'jest-preset-angular/presets/defaults-esm',
-  globals: {
-    'ts-jest': {
-      useESM: true,
-      stringifyContentPathRegex: '\\.(html|svg)$',
-      tsconfig: '<rootDir>/tsconfig-esm.spec.json',
-    },
-  },
-  moduleNameMapper: {
-    ...pathsToModuleNameMapper(paths, { prefix: '<rootDir>' }),
-    tslib: 'tslib/tslib.es6.js',
-  },
+  preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  globalSetup: 'jest-preset-angular/global-setup',
 };

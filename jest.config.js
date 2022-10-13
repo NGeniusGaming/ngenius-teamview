50958b892ddf7945e707db5ba58ac17456fdc15f
Brandon Ward <brandon.ward1993@gmail.com>
2022-10-12 21:51:07 -0600

    Update jest-preset-angular

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,21 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { paths } = require('./tsconfig.json').compilerOptions;
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  moduleNameMapper: {
-    '@core/(.*)': '<rootDir>/src/app/core/$1',
+  preset: 'jest-preset-angular/presets/defaults-esm',
+  globals: {
+    'ts-jest': {
+      useESM: true,
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      tsconfig: '<rootDir>/tsconfig-esm.spec.json',
+    },
   },
-  preset: 'jest-preset-angular',
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(paths, { prefix: '<rootDir>' }),
+    tslib: 'tslib/tslib.es6.js',
+  },
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
 };
